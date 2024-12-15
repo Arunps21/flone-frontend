283aa373633337ae146d794cc8ca51cf5cabdc7e
arun <arunps880@gmail.com>
2024-12-15 21:47:19 +0530

    nav update

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,11 +1,7 @@
-import cart from './cart_icon.png'
-import profile from './profile_icon.png'
-import menu from './menu_icon.png'
-import search from './search_icon.png'
+import cart from "./cart_icon.png";
+import profile from "./profile_icon.png";
+import menu from "./menu_icon.png";
+import search from "./search_icon.png";
+import cross from "./cross_icon.png";
 
-export {
-    cart,
-    profile,
-    menu,
-    search
-}
+export { cart, profile, menu, search, cross };
--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { navIcon } from "../assets";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+// import { navIcon } from "../assets";
+import { Link, NavLink } from "react-router-dom";
+import { cart, cross, menu, profile, search } from "../assets/icons/icons";
 
 const Nav = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <nav className="flex justify-between items-center py-5 font-medium">
-      <h1 className="text-2xl font-bold uppercase">Flone.</h1>
+      {/* name */}
+      <a href="" className="text-3xl font-bold uppercase">
+        Flone.
+      </a>
+
+      {/* navbar routes */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 uppercase">
         <NavLink to="/">
           <p className="hover:text-coral-red">Home</p>
@@ -20,17 +27,73 @@ const Nav = () => {
           <p className="hover:text-coral-red">contact us</p>
         </NavLink>
       </ul>
-      <div className="flex gap-6">
+      {/* <div className="flex items-center gap-6">
         {navIcon &&
           navIcon.map((icon, index) => (
             <a href={icon.href} key={index} rel="noopener noreferrer">
               <img
                 src={icon.icon}
                 alt={icon.altText || "Navigation icon"}
-                width={25}
+                className="w-5 cursor-pointer"
               />
             </a>
           ))}
+      </div> */}
+
+      {/* icons */}
+      <div className="flex items-center gap-6">
+        <img src={search} alt="Search Icon" className="w-5 cursor-pointer" />
+
+        <div className="group relative">
+          <img
+            src={profile}
+            alt="Profile Icon"
+            className="w-5 cursor-pointer"
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-2 px-5 bg-slate-50 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-coral-red">My Account</p>
+              <hr />
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <hr />
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="" className="relative">
+          <img src={cart} alt="" className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            5
+          </p>
+        </Link>
+        <img
+          onClick={() => setVisible(true)}
+          src={menu}
+          alt=""
+          className="w-5 cursor-pointer sm:hidden"
+        />
+      </div>
+
+      {/* sidebar menu */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600 relative">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center absolute gap-4 p-3 right-4 top-2 cursor-pointer"
+          >
+            <img src={cross} alt="" className="h-4 " />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-6 text-lg mt-28">
+          <NavLink onClick={()=>setVisible(false)} to="/">Home</NavLink>
+          <NavLink onClick={()=>setVisible(false)} to="">About</NavLink>
+          <NavLink onClick={()=>setVisible(false)} to="">Products</NavLink>
+          <NavLink onClick={()=>setVisible(false)} to="">Contact us</NavLink>
+        </div>
       </div>
     </nav>
   );
